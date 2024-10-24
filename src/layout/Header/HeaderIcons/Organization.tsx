75ec42754e207f5f8a56d4a48fd34c5d4038fcfc
AmanRasheed-cygnonex <amanrasheed@cygnonex.com>
2024-10-24 10:40:31 +0530

    Profile View

--- src/layout/Header/HeaderIcons/Organization.tsx
+++ src/layout/Header/HeaderIcons/Organization.tsx
@@ -1,36 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import OrganizationIcon from "../../../assets/icons/OrganizationIcon";
 import SettingsIcons from "../../../assets/icons/SettingsIcon";
 import Drawer from "../../../Components/drawer/drawer";
 import Button from "../../../Components/Button";
-import { useNavigate } from "react-router-dom";
 import Modal from "../../../Components/model/Modal";
+import { endponits } from "../../../Services/apiEndpoints";
+import useApi from "../../../Hooks/useApi";
+import toast from "react-hot-toast";
 
 type Props = {};
 
-function Organization({}: Props) {
+const Organization = ({}: Props) => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
-  const [isLogoutModalOpen, setLogoutModalOpen] = useState(false); // State for controlling the modal
+  const [isLogoutModalOpen, setLogoutModalOpen] = useState(false);
+  const [organizationData, setOrganizationData] = useState<any>(null);
+  const { request: getOneOrganization } = useApi("get", 5004);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getOrganization();
+  }, []);
+
+  const getOrganization = async () => {
+    try {
+      const url = `${endponits.GET_ONE_ORGANIZATION}`;
+      const apiResponse = await getOneOrganization(url);
+      setOrganizationData(apiResponse.response?.data);
+    } catch (error) {
+      toast.error("Error fetching organization");
+      console.error("Error fetching organization:", error);
+    }
+  };
 
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
   };
 
-  const navigate = useNavigate();
-
-  // Open modal for logout confirmation
   const confirmLogout = () => {
     setLogoutModalOpen(true);
   };
 
-  // Handle actual logout action
   const handleLogout = () => {
     localStorage.clear();
     navigate("/login");
-    setLogoutModalOpen(false); // Close modal after logging out
+    setLogoutModalOpen(false);
   };
 
-  // Close the modal without logging out
   const closeModal = () => {
     setLogoutModalOpen(false);
   };
@@ -43,13 +60,9 @@ function Organization({}: Props) {
 
       <Drawer onClose={toggleDrawer} open={isDrawerOpen} position="right">
         <div className="flex items-center justify-between p-5">
-          <h5 className="text-md font-semibold text-gray-700">
-            My Organizations
-          </h5>
+          <h5 className="text-md font-semibold text-gray-700">My Organizations</h5>
           <div className="flex gap-2 items-center">
-            <div>
-              <SettingsIcons size="sm" />
-            </div>
+            <SettingsIcons size="sm" />
             <button
               onClick={toggleDrawer}
               className="font-normal text-textColor hover:text-gray-700 text-3xl -mt-1"
@@ -59,32 +72,37 @@ function Organization({}: Props) {
           </div>
         </div>
         <div className="p-4 space-y-4">
-          {["Company branch 1", "Company branch 2", "Company branch 3"].map((branch, index) => (
-            <div key={index} className="flex items-center border border-slate-200 p-4 rounded-md shadow-sm">
-              <div className="mr-4">
-                <OrganizationIcon />
+          {organizationData ? (
+            <div className="flex flex-col border border-slate-200 p-4 rounded-md shadow-sm">
+              <div className="flex items-center mb-4">
+                <OrganizationIcon  />
+                <div className="flex-grow">
+                  <h5 className="font-bold text-sm text-gray-700">{organizationData.organizationName}</h5>
+                  <p className="text-xs text-gray-700">Organization ID: {organizationData.organizationId}</p>
+                </div>
               </div>
-              <div className="flex-grow">
-                <h5 className="font-bold text-sm text-gray-700">{branch}</h5>
-                <p className="text-xs text-gray-700">
-                  Organization ID: 43434659883
-                </p>
-              </div>
-              <div className="flex items-center">
-                <span className="text-xs bg-yellow-100 border text-yellow-800 py-1 px-2 rounded-2xl pl-2 pr-2">
-                  Trail
-                </span>
-              </div>
-              <div className="ml-24">
-                <input
-                  type="radio"
-                  name="selectedBranch"
-                  className="h-4 w-4"
-                />
-              </div>
+              <p className="text-xs text-gray-700">Address Line 1: {organizationData.addline1}</p>
+              <p className="text-xs text-gray-700">Address Line 2: {organizationData.addline2}</p>
+              <p className="text-xs text-gray-700">City: {organizationData.city}</p>
+              <p className="text-xs text-gray-700">State: {organizationData.state}</p>
+              <p className="text-xs text-gray-700">Country: {organizationData.organizationCountry}</p>
+              <p className="text-xs text-gray-700">Pincode: {organizationData.pincode}</p>
+              <p className="text-xs text-gray-700">Primary Contact Name: {organizationData.primaryContactName}</p>
+              <p className="text-xs text-gray-700">Primary Contact Number: {organizationData.primaryContactNum}</p>
+              <p className="text-xs text-gray-700">Phone Number: {organizationData.organizationPhNum}</p>
+              <p className="text-xs text-gray-700">Industry: {organizationData.organizationIndustry}</p>
+              <p className="text-xs text-gray-700">Base Currency: {organizationData.baseCurrency}</p>
+              <p className="text-xs text-gray-700">Date Format: {organizationData.dateFormat}</p>
+              <p className="text-xs text-gray-700">Date Format Example: {organizationData.dateFormatExp}</p>
+              <p className="text-xs text-gray-700">Date Split: {organizationData.dateSplit}</p>
+              <p className="text-xs text-gray-700">Fiscal Year: {organizationData.fiscalYear}</p>
+              <p className="text-xs text-gray-700">Time Zone: {organizationData.timeZone}</p>
+              <p className="text-xs text-gray-700">Time Zone Example: {organizationData.timeZoneExp}</p>
             </div>
-          ))}
-          <div className="flex justify-center items-center">
+          ) : (
+            <p className="text-xs text-gray-700">Loading...</p>
+          )}
+          <div className="flex justify-end">
             <Button className="pl-10 pr-10 h-[34px] text-sm" onClick={confirmLogout}>
               Logout
             </Button>
@@ -95,25 +113,15 @@ function Organization({}: Props) {
       {isLogoutModalOpen && (
         <Modal
           open
-          onClose={closeModal} 
+          onClose={closeModal}
           className="rounded-lg p-8 w-[546px] h-[160px] text-[#303F58] space-y-8"
         >
-          <p className="text-sm">
-            Are you sure you want to log out?
-          </p>
+          <p className="text-sm">Are you sure you want to log out?</p>
           <div className="flex justify-end gap-2 mb-3">
-            <Button
-              onClick={closeModal} 
-              variant="secondary"
-              className="pl-8 pr-8 text-sm h-10"
-            >
+            <Button onClick={closeModal} variant="secondary" className="pl-8 pr-8 text-sm h-10">
               Cancel
             </Button>
-            <Button
-              onClick={handleLogout} 
-              variant="primary"
-              className="pl-8 pr-8 text-sm h-10"
-            >
+            <Button onClick={handleLogout} variant="primary" className="pl-8 pr-8 text-sm h-10">
               Ok
             </Button>
           </div>
@@ -121,6 +129,6 @@ function Organization({}: Props) {
       )}
     </>
   );
-}
+};
 
 export default Organization;
