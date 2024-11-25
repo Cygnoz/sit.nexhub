627c5bdfd998ebafb83e7baa049ebf4fb3fb4bfe
Alwin-Cygno <alwinsathish260196@gmial.com>
2024-11-25 17:43:20 +0530

    new

--- src/features/sales/Receipt/NewRecieptTable.tsx
+++ src/features/sales/Receipt/NewRecieptTable.tsx
@@ -3,95 +3,95 @@ import toast from "react-hot-toast";
 import { SalesRecieptTable } from "../../../assets/constants";
 
 type Props = {
-    customerReciept?: InvoiceType[];
-    recieptState?: any;
-    setRecieptState?: any;
+  customerReciept?: InvoiceType[];
+  recieptState?: any;
+  setRecieptState?: any;
 }
 type InvoiceType = {
-    invoiceId: string;
-    salesInvoice: string;
-    salesInvoiceDate: string;
-    dueDate: string;
-    totalAmount: number;
-    balanceAmount: number;
-    paymentAmount: number;
-    paidStatus: string;
+  invoiceId: string;
+  salesInvoice: string;
+  salesInvoiceDate: string;
+  dueDate: string;
+  totalAmount: number;
+  balanceAmount: number;
+  paymentAmount: number;
+  paidStatus: string;
 };
 
 function NewRecieptTable({ customerReciept = [], recieptState, setRecieptState }: Props) {
 
-    const [data, setData] = useState<InvoiceType[]>([
-        {
-            invoiceId: "",
-            salesInvoice: "",
-            salesInvoiceDate: "",
-            dueDate: "",
-            totalAmount: 0,
-            balanceAmount: 0,
-            paymentAmount: 0,
-            paidStatus:""
-        },
-    ]);
+  const [data, setData] = useState<InvoiceType[]>([
+    {
+      invoiceId: "",
+      salesInvoice: "",
+      salesInvoiceDate: "",
+      dueDate: "",
+      totalAmount: 0,
+      balanceAmount: 0,
+      paymentAmount: 0,
+      paidStatus: ""
+    },
+  ]);
 
-    useEffect(() => {
-        if (customerReciept && Array.isArray(customerReciept)) {
-            const filteredInvoice = customerReciept?.filter(
-                (invoice: any) => invoice.paidStatus === "Pending" || invoice.paidStatus === "Overdue"
-            );
+  useEffect(() => {
+    if (customerReciept && Array.isArray(customerReciept)) {
+      const filteredInvoice = customerReciept?.filter(
+        (invoice: any) => invoice.paidStatus === "Pending" || invoice.paidStatus === "Overdue"
+      );
 
-            setData(
-                filteredInvoice.map((invoice: any) => ({
-                    invoiceId: invoice.invoiceId || "",
-                    salesInvoice: invoice.salesInvoice || "",
-                    salesInvoiceDate: invoice.salesInvoiceDate || "",
-                    dueDate: invoice.dueDate || "",
-                    totalAmount: invoice.totalAmount || 0,
-                    balanceAmount: invoice.balanceAmount || 0,
-                    paymentAmount: invoice.paymentAmount || 0,
-                    paidStatus:invoice.paidStatus || "",
-                }))
-            );
-        }
-    }, [customerReciept]);
+      setData(
+        filteredInvoice.map((invoice: any) => ({
+          invoiceId: invoice._id || "",
+          salesInvoice: invoice.salesInvoice || "",
+          salesInvoiceDate: invoice.salesInvoiceDate || "",
+          dueDate: invoice.dueDate || "",
+          totalAmount: invoice.totalAmount || 0,
+          balanceAmount: invoice.balanceAmount || 0,
+          paymentAmount: invoice.paymentAmount || 0,
+          paidStatus: invoice.paidStatus || "",
+        }))
+      );
+    }
+  }, [customerReciept]);
 
-    const handleRowChange = (
-        index: number,
-        field: keyof InvoiceType,
-        value: string | number
-    ) => {
-        const newData = [...data];
-        const invoiceAmount = newData[index].totalAmount;
-        let paymentValue = typeof value === "number" ? value : parseFloat(value);
+  const handleRowChange = (
+    index: number,
+    field: keyof InvoiceType,
+    value: string | number
+  ) => {
+    const newData = [...data];
+    const invoiceAmount = newData[index].balanceAmount;
+    let paymentValue = typeof value === "number" ? value : parseFloat(value);
 
-        if (field === "paymentAmount") {
-            if (paymentValue > invoiceAmount) {
-                toast.error(
-                    `Payment cannot exceed the total amount of ${invoiceAmount}. Setting payment to total amount.`
-                );
-                paymentValue = invoiceAmount;
-            }
-            newData[index][field] = paymentValue;
-        }
+    if (field === "paymentAmount") {
+      if (paymentValue > invoiceAmount) {
+        toast.error(
+          `Payment cannot exceed the total amount of ${invoiceAmount}. Setting payment to total amount.`
+        );
+        paymentValue = invoiceAmount;
+      }
+      newData[index][field] = paymentValue;
+    }
 
-        setData(newData);
+    setData(newData);
 
-        if (setRecieptState) {
-            const totalPayment = newData.reduce(
-                (acc, row) => acc + (row.paymentAmount || 0),
-                0
-            );
-            setRecieptState((prevState: any) => ({
-                ...prevState,
-                invoices: newData,
-                total: totalPayment,
-                amountPaid: totalPayment,
-                amountUsedForPayments: totalPayment,
-            }));
-        }
-    };
+    if (setRecieptState) {
+      const totalPayment = newData.reduce(
+        (acc, row) => acc + (row.paymentAmount || 0),
+        0
+      );
+      setRecieptState((prevState: any) => ({
+        ...prevState,
+        invoice: newData,
+        total: totalPayment,
+        amountReceived: totalPayment,
+        amountUsedForPayments: totalPayment,
+      }));
+    }
+  };
 
-    return (
-<div>
+  return (
+    <div>
       <div className="rounded-lg border-2 border-tableBorder mt-3">
         <table className=" text-xs min-w-full bg-white rounded-lg relative pb-4 border-dropdownText text-textColor">
           <thead className="text-[12px] text-center text-dropdownText">
@@ -145,9 +145,9 @@ function NewRecieptTable({ customerReciept = [], recieptState, setRecieptState }
                         type="text"
                         placeholder="Bill ID"
                         className="w-full focus:outline-none text-center"
-                        value={row.invoiceId ? row.invoiceId : "-"}
+                        value={row.salesInvoice ? row.salesInvoice : "-"}
                         onChange={(e) =>
-                          handleRowChange(index, "invoiceId", e.target.value)
+                          handleRowChange(index, "salesInvoice", e.target.value)
                         }
                       />
                     </td>
@@ -211,10 +211,10 @@ function NewRecieptTable({ customerReciept = [], recieptState, setRecieptState }
         </table>
       </div>
       <p className="text-right text-textColor text-sm mt-4">
-        Total <span className="ms-20 font-semibold">{recieptState.total}</span>
+        Total <span className="ms-20 font-semibold">{recieptState.total ? recieptState.total : "0"}</span>
       </p>
     </div>
-    )
+  )
 }
 
 export default NewRecieptTable
