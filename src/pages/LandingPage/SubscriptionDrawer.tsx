e37f387360288fe7c96b7485ec32a56462330522
Alwin-Cygno <alwin.sathish@cygnonex.com>
2025-02-23 22:33:56 +0530

    new

--- src/pages/LandingPage/SubscriptionDrawer.tsx
+++ src/pages/LandingPage/SubscriptionDrawer.tsx
@@ -27,9 +27,15 @@ function SubscriptionDrawer({ mode }: Props) {
         onClose={toggleDrawer}
         open={isDrawerOpen}
         position="right"
-        style={{ backgroundColor: !mode ? "#14181B" :"#FEFDFA", width: "34%" }}
+        style={{ backgroundColor: !mode ? "#14181B" : "#FEFDFA", width: "34%" }}
       >
-        <div className={`${!mode ? "bg-[#21282C] text-[#FCF8ED]" :"bg-[#FDF8F0] text-[#4B5C79]"} p-8`}>
+        <div
+          className={`${
+            !mode
+              ? "bg-[#21282C] text-[#FCF8ED]"
+              : "bg-[#FDF8F0] text-[#4B5C79]"
+          } p-8`}
+        >
           <p
             className="text-[#9EA9BB] text-3xl absolute right-4 top-2 cursor-pointer font-light"
             onClick={toggleDrawer}
@@ -43,38 +49,53 @@ function SubscriptionDrawer({ mode }: Props) {
               alt=""
             />
             <p className=" font-medium text-xl">360 One Pvt</p>
-            <Button variant={!mode ? "secondary" : "fourthiary"} className="h-8 pl-7 pr-7">
+            <Button
+              variant={!mode ? "secondary" : "fourthiary"}
+              className="h-8 pl-7 pr-7"
+            >
               Trial
             </Button>
           </div>
         </div>
 
-        <div className={`p-12 ${!mode ? "text-white" :"text-[#4B5C79]"}`}>
-          <div className={`mb-10 flex justify-between items-center border-b border-[#2C353B] pb-5`}>
+        <div className={`p-12 ${!mode ? "text-white" : "text-[#4B5C79]"}`}>
+          <div
+            className={`${
+              !mode ? "border-[#2C353B]" : "border-[#BEC0C2]"
+            } mb-10 flex justify-between items-center border-b  pb-5`}
+          >
             <p className=" text-base">Type of subscription</p>
             <p className="text-base font-medium">Trial</p>
           </div>
-          <div className="mb-10 flex justify-between items-center border-b border-[#2C353B] pb-5">
+          <div
+            className={`${
+              !mode ? "border-[#2C353B]" : "border-[#BEC0C2]"
+            } mb-10 flex justify-between items-center border-b  pb-5`}
+          >
             <p className=" text-base">Start Date</p>
             <p className="text-base font-medium">20 June 2024</p>
           </div>
-          <div className="mb-7 flex justify-between items-center border-b border-[#2C353B] pb-5">
+          <div
+            className={`${
+              !mode ? "border-[#2C353B]" : "border-[#BEC0C2]"
+            } mb-7 flex justify-between items-center border-b  pb-5`}
+          >
             <p className=" text-base">End Date</p>
             <p className="text-base font-medium">28 June 2024</p>
           </div>
         </div>
 
         <div className="p-6">
-          <p className="text-[#FCF8ED] text-base font-medium mb-4">
+          <p className={`${mode ? "text-[#4B5C79]":"text-[#FCF8ED]"}  text-base font-medium mb-4`}>
             Point of Contact
           </p>
-          <div className="bg-[#21282C] p-6 rounded-xl flex items-center gap-5">
+          <div className={`${!mode ? "bg-[#21282C]":"bg-[#FDF8F0]"} " p-6 rounded-xl flex items-center gap-5`}>
             <img
               src={defaultImage}
               className="w-14 h-14 rounded-full object-cover"
               alt=""
             />
-            <div className="text-[#FEFDFA]">
+            <div className={`${mode ? "text-[#4B5C79]":"text-[#FEFDFA]"} `}>
               <p className=" font-medium text-lg mb-2">Ethan Thompson</p>
               <p className="text-base">
                 8017987647 &nbsp; | &nbsp; ethanth2@gmail.com
