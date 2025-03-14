d5d8455ca42c766f616324a9145e09c974ba5848
Anju <anjup3994@gmail.com>
2025-03-14 15:05:32 +0530

    new

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ server.use(reportRouter)
 const PORT = 7006;
 
 server.get('/',(req,res)=>{
-    res.status(200).json("Dev Bill BIZZ server started - Report v1.6")
+    res.status(200).json("Dev NexHub server started - Report v1.6")
 })
 
 server.listen(PORT,()=>{
-    console.log(`BillBIZZ server Report started at port : ${PORT}`);
+    console.log(`NexHub server Report started at port : ${PORT}`);
 
 })
