ce7d2f04eebae8f3f086a2bef2880b09750c3472
Anju <anjup3994@gmail.com>
2025-03-17 12:28:10 +0530

    new

--- router/inventoryRouter.js
+++ router/inventoryRouter.js
@@ -76,7 +76,7 @@ router.get('/get-item-transaction/:id',verifyToken,checkPermission('Viewed Item 
 
 //nexPortal
 // Item
-router.get('/getallitemnexportal', nexVerifyToken, itemController.getAllItem);
+router.get('/get-all-item-nexportal', nexVerifyToken, itemController.getAllItem);
 
 
 
