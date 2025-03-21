e2b3d644593cf8540b474b83dc51871adaf0609d
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-03-21 10:00:39 +0530

    new

--- server.js
+++ server.js
@@ -29,6 +29,17 @@ server.options('*', (req, res) => {
 server.use(helmet()); 
 server.use(staffRouter,expenseRouter);
 
+// Catch unhandled promise rejections
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Promise Rejection:", reason);
+});
+
+// Catch uncaught exceptions
+process.on("uncaughtException", (error) => {
+    console.error("Uncaught Exception:", error);
+    process.exit(1); // Optionally restart the server
+});
+
 // General error handling middleware
 server.use((err, req, res, next) => {
     console.error(err.stack);
