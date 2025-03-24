1a7ec102823c2f40aee7595996cae6d1f2d008fb
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-03-24 11:13:26 +0530

    new

--- server.js
+++ server.js
@@ -6,25 +6,40 @@ const helmet = require('helmet');
 
 const passport = require("passport");
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",");
+
 const server = express();
 const organizationRouter = require('./router/organizationRouter');
 
 require('./database/connection/connection');
 require("./controller/azure/authController");
 
+
 server.use(express.json({ limit: '10mb' })); 
 server.use(express.urlencoded({ limit: '10mb', extended: true }));
 
+
 server.use(cors({
-    origin: "*",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS not allowed from this origin"));
+        }
+    },
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Content-Type, Authorization"
 }));
 
+
+// Handle CORS preflight requests safely
 server.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    const origin = req.headers.origin;
+    if (!origin || allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    }
     res.sendStatus(200);
 });
 
@@ -32,16 +47,19 @@ server.use(helmet());
 server.use(express.json());
 server.use(organizationRouter);
 
+
 // Catch unhandled promise rejections
 process.on("unhandledRejection", (reason, promise) => {
     console.error("Unhandled Promise Rejection:", reason);
 });
+
 
 // Catch uncaught exceptions
 process.on("uncaughtException", (error) => {
     console.error("Uncaught Exception:", error);
     process.exit(1); // Optionally restart the server
 });
+
 
 // General error handling middleware
 server.use((err, req, res, next) => {
