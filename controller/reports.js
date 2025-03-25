2223aa5dd937688cb5f9bf6fd9b56cd396a8f6e4
MohdThaha <165124426+MohdThaha@users.noreply.github.com>
2025-03-25 13:01:11 +0530

    new

--- controller/reports.js
+++ controller/reports.js
@@ -79,7 +79,7 @@ async function getReportAccount(organizationExists,organizationId, startDate, en
                 $match: { 
                     "accountDetails.accountSubhead": accountSubHead,
                     "accountDetails.accountName": { 
-                        $nin: ["Sales Discount", "Purchase Discount","Sales Discount(Cash Discount)","Purchase Discount(Cash Discount)"] 
+                        $nin: ["Sales Discount", "Purchase Discount","Sales Discount(Cash Discount)","Purchase Discounts(Cash Discount)"] 
                     } 
                 } 
             },
@@ -132,7 +132,7 @@ async function getReportAccount(organizationExists,organizationId, startDate, en
                 $match: { 
                     "accountDetails.accountSubhead": accountSubHead,
                     "accountDetails.accountName": { 
-                        $nin: ["Sales Discount", "Purchase Discount","Sales Discount(Cash Discount)","Purchase Discount(Cash Discount)"] 
+                        $nin: ["Sales Discount", "Purchase Discount","Sales Discount(Cash Discount)","Purchase Discounts(Cash Discount)"] 
                     } 
                 } 
             },
