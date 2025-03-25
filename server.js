613e76047a1ebdf7b0d18502836a6a8fc1ec72fb
Anju <anjup3994@gmail.com>
2025-03-25 19:13:21 +0530

    new

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ server.use((err, req, res, next) => {
 const PORT = 7001;
 
 server.get('/',(req,res)=>{
-    res.status(200).json("Dev NexHub server started - Accounts v1.6")
+    res.status(200).json("Dev NexHub server started")
 })
 
 server.listen(PORT,()=>{
