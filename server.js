2d5489d41bb60e6d12cd19115f2658139cc4bbc9
unknown <amaljoy@cygnonex.com>
2025-03-26 15:03:06 +0530

    v1

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ server.use((err, req, res, next) => {
 const PORT = 7006;
 
 server.get('/',(req,res)=>{
-    res.status(200).json("Dev NexHub server started - Report v1.6")
+    res.status(200).json("Sit NexHub server started - Report v2")
 })
 
 server.listen(PORT,()=>{
-    console.log(`NexHub server Report started at port : ${PORT}`);
+    console.log(`NexHub Sit server Report started at port : ${PORT}`);
 
 })
