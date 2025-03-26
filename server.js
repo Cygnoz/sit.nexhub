eeac64fbb33b3541587f9a47aeaaa00fa81a43a8
unknown <amaljoy@cygnonex.com>
2025-03-26 14:50:40 +0530

    v1

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ server.use((err, req, res, next) => {
 const PORT = 7003
 
 server.get('/',(req,res)=>{
-    res.status(200).json("Dev NexHub server started - Inventory v2.0")
+    res.status(200).json("Sit NexHub server started - Inventory v2")
 });
 
 server.listen(PORT,()=>{
-    console.log(`NexHub server Inventory started at port : ${PORT}`);
+    console.log(`NexHub Sit server Inventory started at port : ${PORT}`);
 });
 
